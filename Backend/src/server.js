720e34a4ad2062d1e3154115ad8f8def09e971f5
Prashu18-Coder <saiprasanth1425@gmail.com>
2026-02-27 22:42:53 +0530

    production changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,14 +3,19 @@ import notesRoutes from "./Routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "./MiddleWare/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 // import dotenv from "dotenv";
 
 // dotenv.config();
 // console.log(process.env.MONGO_URL);
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors());
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors());
+}
+
 app.use(express.json()); // Middle-ware
 app.use(rateLimiter);
 // app.use((req,res,next)=>{
@@ -19,7 +24,13 @@ app.use(rateLimiter);
 // });
 app.use("/api/notes",notesRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
 
+    app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+});
+}
 connectDB().then( () => {
     app.listen(PORT, ()=> {
     console.log("Server started on PORT :",PORT);
